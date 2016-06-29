0ebc307a3f3847812e6cba161dafcb827dfcaf1b
Keith <TheKrush@users.noreply.github.com>
2016-06-29 23:23:19 +0000

    resharper settings code clean-up

--- TilesGenerator/Properties/AssemblyInfo.cs
+++ TilesGenerator/Properties/AssemblyInfo.cs
@@ -15,13 +15,14 @@
     You should have received a copy of the GNU General Public License
     along with TilesGenerator.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("TilesGenerator")]
 [assembly: AssemblyDescription("http://blog.mikecouturier.com")]
 [assembly: AssemblyConfiguration("")]
@@ -34,9 +35,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("c363b794-28fe-4646-864a-be266cbc7d7d")]
 
 // Version information for an assembly consists of the following four values:
@@ -49,5 +52,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
