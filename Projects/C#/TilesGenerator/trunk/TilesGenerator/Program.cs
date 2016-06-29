58725dd773fa087929b8d846cc777d4c7fa13db7
Krush <Krush@5c276bda-50ee-4912-b116-757201202087>
2016-06-29 23:23:19 +0000

    resharper settings code clean-up

--- Projects/C#/TilesGenerator/trunk/TilesGenerator/Program.cs
+++ Projects/C#/TilesGenerator/trunk/TilesGenerator/Program.cs
@@ -15,76 +15,27 @@
     You should have received a copy of the GNU General Public License
     along with TilesGenerator.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
+using System.IO;
 
 namespace blog_mikecouturier_com
 {
     class Program
     {
-        const int TILE_SIZE = 256;
-        const string TILE_FILENAME = @"{0}\{1}\{2}.png";
+        #region Constants
+
         const string OUTPUT_DIR = @"output\";
+        const string TILE_FILENAME = @"{0}\{1}\{2}.png";
+        const int TILE_SIZE = 256;
 
-        static void Main(string[] args)
-        {
-            int level = 0;
-
-            if (args.Length != 2 || !Int32.TryParse(args[0], out level) || level < 1 || level > 5 || !File.Exists(args[1]))
-            {
-                Console.WriteLine("usage: \"TilesGenerator.exe [1-5] [filename]\"");
-                return;
-            }
-
-            try
-            {
-                int mapWidth = GetMapWidth(level);
-
-                if (!Directory.Exists(OUTPUT_DIR))
-                    Directory.CreateDirectory(OUTPUT_DIR);
-
-                using (Stream stream = File.OpenRead(args[1]))
-                using (Image original = Bitmap.FromStream(stream))
-                {
-                    SplitTilesRecursive(original, level);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-
-        static void SplitTilesRecursive(Image original, int level)
-        {
-            int mapWidth = GetMapWidth(level);
-            int tilesOnSide = mapWidth / TILE_SIZE;
-
-            using (Image resized = ResizeImage(original, new Size(mapWidth, mapWidth)))
-            {
-                for (int x = 0; x < tilesOnSide; x++)
-                    for (int y = 0; y < tilesOnSide; y++)
-                        CropAndSaveTile(resized, x, y, level);
-            }
-
-            if (level > 0)
-                SplitTilesRecursive(original, level - 1);
-        }
-
-        static int GetMapWidth(int level)
-        {
-            return TILE_SIZE * (int)Math.Pow(2, level);
-        }
+        #endregion
 
         private static void CropAndSaveTile(Image image, int x, int y, int level)
         {
-            Rectangle cropArea = new Rectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            Rectangle cropArea = new Rectangle(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE);
 
             using (Bitmap bmpImage = new Bitmap(image))
             using (Bitmap bmpCrop = bmpImage.Clone(cropArea, bmpImage.PixelFormat))
@@ -100,17 +51,68 @@ namespace blog_mikecouturier_com
             }
         }
 
+        static int GetMapWidth(int level)
+        {
+            return TILE_SIZE*(int) Math.Pow(2, level);
+        }
+
+        static void Main(string[] args)
+        {
+            int level = 0;
+
+            if (args.Length != 2 || !Int32.TryParse(args[0], out level) || level < 1 || level > 5 ||
+                !File.Exists(args[1]))
+            {
+                Console.WriteLine("usage: \"TilesGenerator.exe [1-5] [filename]\"");
+                return;
+            }
+
+            try
+            {
+                int mapWidth = GetMapWidth(level);
+
+                if (!Directory.Exists(OUTPUT_DIR))
+                    Directory.CreateDirectory(OUTPUT_DIR);
+
+                using (Stream stream = File.OpenRead(args[1]))
+                using (Image original = Image.FromStream(stream))
+                {
+                    SplitTilesRecursive(original, level);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
         private static Image ResizeImage(Image toResize, Size size)
         {
             Bitmap b = new Bitmap(size.Width, size.Height);
 
-            using (Graphics g = Graphics.FromImage((Image)b))
+            using (Graphics g = Graphics.FromImage((Image) b))
             {
                 g.InterpolationMode = InterpolationMode.Default;
                 g.DrawImage(toResize, 0, 0, size.Width, size.Height);
 
-                return (Image)b;
+                return (Image) b;
             }
+        }
+
+        static void SplitTilesRecursive(Image original, int level)
+        {
+            int mapWidth = GetMapWidth(level);
+            int tilesOnSide = mapWidth/TILE_SIZE;
+
+            using (Image resized = ResizeImage(original, new Size(mapWidth, mapWidth)))
+            {
+                for (int x = 0; x < tilesOnSide; x++)
+                    for (int y = 0; y < tilesOnSide; y++)
+                        CropAndSaveTile(resized, x, y, level);
+            }
+
+            if (level > 0)
+                SplitTilesRecursive(original, level - 1);
         }
     }
 }
